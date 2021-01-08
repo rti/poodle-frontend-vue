6e67414d30c1db71ceee2ca1c6b62ed94289657a
Robert Timm <mail@rtti.de>
2021-01-08 22:28:38 +0100

    add logo

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
       <div class="level is-mobile px-2 py-2">
         <div class="level-left">
           <a href="/">
-            <h1 class="level-item title">Poodle</h1>
+            <img src="@/assets/logo.svg" width="100px" />
+            <img src="@/assets/textlogo.svg" width="210px" />
           </a>
         </div>
         <div class="level-right">
