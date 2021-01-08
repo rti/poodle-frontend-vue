7e1fcbcd4d971eb5aed641b8260bc1e7660e1f16
Robert Timm <mail@rtti.de>
2021-01-08 01:01:35 +0100

    make InputTextVerbose reusable

--- src/components/InputTextVerbose.vue
+++ src/components/InputTextVerbose.vue
@@ -1,31 +1,106 @@
 <template>
-<div>
-  <h1 class="title">Schedule an event</h1>
   <div class="field">
+
+    <label v-if="label" class="label">{{ label }}</label>
+
     <p class="control is-medium has-icons-right"
-        v-bind:class="{ 'is-loading': queryName.loading }">
-      <input @input="handleInput"
-          class="input is-medium" type="text" placeholder="Name your event" />
-      <span v-if="queryName.saved" class="icon is-right">
+        v-bind:class="{ 'is-loading': processing }">
+
+      <input type="text"
+          class="input is-medium"
+          v-bind:class="{ 'is-danger': error }"
+          :placeholder="placeholder"
+          @input="handleInput" />
+
+      <span v-if="done" class="icon is-right">
+      <!-- TODO we could use a slot here -->
         OK
       </span>
+
     </p>
+
+    <div class="help-container">
+      <span v-if="done && successMsg" class="help is-success">{{ successMsg }}</span>
+      <span v-if="error && errorMsg" class="help is-danger">{{ errorMsg }}</span>
+    </div>
+
   </div>
-</div>
+
 </template>
 
 /******************************************************************************/
 
 <script>
 export default {
+  name: 'InputTextVerbose',
+  props: {
+    label: String,
+    placeholder: String,
+    successMsg: String,
+    required: {
+      type: String, // so we do not need to use v-bind to force bool
+      default: null,
+    },
+    requiredMsg: {
+      type: String,
+      default: 'Value required',
+    },
+    processFunc: Function,
+    debounce: {
+      type: Number,
+      default: 250,
+    },
+  },
+
   data() {
     return {
-      queryName: this.$store.state.query.name,
+      processing: false,
+      done: false,
+      error: false,
+      errorMsg: '',
+      timeout: undefined,
     }
   },
+
   methods: {
     handleInput(e) {
-      this.$store.setQueryName(e.target.value);
+      let value = e.target.value;
+
+      this.done = false;
+      this.error = false;
+      this.errorMsg = '';
+      this.processing = false;
+
+      if(!this.processFunc) {
+        return;
+      }
+
+      if(this.timeout) {
+        clearTimeout(this.timeout);
+      }
+
+      if(this.required && !value) {
+        this.error = true;
+        this.errorMsg = this.requiredMsg;
+        return;
+      }
+
+      this.processing = true;
+
+      this.timeout = setTimeout(async () => {
+          try {
+            await this.processFunc(value);
+            this.done = true;
+          }
+          catch(e) {
+            this.error = true;
+            this.errorMsg = e;
+            console.log(e);
+          }
+
+          this.processing = false;
+
+      }, this.debounce);
     }
   }
 };
