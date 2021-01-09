ee216bde85b624a15c7903a2765a545493afb4a9
Robert Timm <mail@rtti.de>
2021-01-09 01:19:54 +0100

    use input label

--- src/components/QueryCreate.vue
+++ src/components/QueryCreate.vue
@@ -2,8 +2,9 @@
   <div>
     <h1 class="title">Create a query</h1>
     <InputTextVerbose
-        placeholder="Give it a name"
         required="true"
+        label="Give it a name"
+        placeholder="e.g. Remote Family Dinner"
         success-msg="Cool, this looks good."
         required-msg="You really need to provide a name here."
         :initialValue="restoredName"
