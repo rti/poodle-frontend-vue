55b9d3d3212873a07808523637d5a4c08395d725
Robert Timm <mail@rtti.de>
2021-01-10 00:14:05 +0100

    cosmetics

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     const contentWithoutStyle = content.replace(/<style[^]+?<\/style>/gi, '')
     return contentWithoutStyle.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
   },
-  safelist: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/ ],
+
+  safelist: [ /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).+-move$/,
+    /^router-link(|-exact)-active$/, /data-v-.*/ ],
 });
 
 const cssnano = require('cssnano')({
--- src/components/QueryCreate.vue
+++ src/components/QueryCreate.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="title">Schedule a new event</h1>
+    <div class="title">Schedule a new event</div>
 
     <InputTextVerbose
         required="true"
