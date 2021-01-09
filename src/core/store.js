8e2fb16b332123d2705e19a367b7007e24949265
Robert Timm <mail@rtti.de>
2021-01-09 01:10:53 +0100

    store is a placeholder for now

--- src/core/store.js
+++ src/core/store.js
@@ -1,69 +1,16 @@
-const baseUrl = 'http://localhost:8000/app/';
-const queryUrl = baseUrl + 'queries/';
-
 const store = {
   debug: true,
 
   state: {
-    query: {
-      name: {
-        abortController: undefined,
-      },
-    },
+    value: undefined,
   },
 
-  apiState: {
-    query: undefined,
-  },
-
-  async setQueryName(value){
+  setValue(value){
     if(this.debug){
-      console.log('setQueryName ', value);
+      console.log('setValue ', value);
     }
 
-    let stateName = this.state.query.name;
-
-    if(!value.length) {
-      return;
-    }
-
-    if(stateName.abortController) {
-      stateName.abortController.abort();
-    }
-
-    let httpMethod;
-    let url;
-
-    if(this.apiState.query) {
-      httpMethod = 'PATCH';
-      url = queryUrl + this.apiState.query.id + '/';
-    }
-    else {
-      httpMethod = 'POST';
-      url = queryUrl;
-    }
-
-    if(this.debug) {
-      console.log('starting ' + httpMethod + ' request to ' + url);
-    }
-
-    stateName.abortController = new AbortController();
-
-    const res = await fetch(url, {
-        method: httpMethod,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 'name': value }),
-        signal: stateName.abortController.signal });
-
-    if(!res.ok) {
-      throw Error(res.statusText);
-    }
-
-    const json = await res.json();
-
-    console.log(json);
-
-    this.apiState.query = json;
+    this.value = value;
   }
 }
 
